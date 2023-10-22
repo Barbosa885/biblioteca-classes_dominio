5b8429b2077dbbbe840ae7ffce28af89c637e92f
hidekitomiyama <145477920+hidekitomiyama@users.noreply.github.com>
2023-10-22 19:47:05 -0300

    Update TUCartao.h

--- include/testes_h/TUCartao.h
+++ include/testes_h/TUCartao.h
@@ -1,20 +1,15 @@
 #ifndef TESTES_H_INCLUDED
 #define TESTES_H_INCLUDED
 
-// Incluir cabeçalhos.
-
-#include "dominios.h"
-#include "entidades.h"
-
 using namespace std;
 
 class TUCartao {
 private:
     const static string CODIGO_VALIDO = "AB12"; 
-    const static string NOME_VALIDO = "Arroz"; 
-    const static string DESCRICAO_VALIDA = "Vamos testar codigos"; 
-    const static string COLUNA_VALIDA = "CONCLUIDO"; 
-    CARTAO *cartao;
+    const static string NOME_VALIDO = "Texto valido"; 
+    const static string DESCRICAO_VALIDA = "Descricao valida"; 
+    const static string COLUNA_VALIDA = "SOLICITADO"; 
+    Cartao *cartao;
     int estado;                             
     void setUp();                           
     void tearDown();                        
