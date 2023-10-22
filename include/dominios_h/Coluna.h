41582b787743c50674be014ab656eb4f1d2e9e02
hidekitomiyama <145477920+hidekitomiyama@users.noreply.github.com>
2023-10-22 09:37:16 -0300

    Update Coluna.h

--- include/dominios_h/Coluna.h
+++ include/dominios_h/Coluna.h
@@ -1,1 +1,21 @@
-
+class COLUNA{
+    private:
+        string coluna;
+        void verifica(string);
+        /** 
+        * verifica se a coluna atende a critérios específicos.
+        * @param a coluna verificada.
+        * saída: "argumento inválido" caso a coluna não atenda a um dos críterios estabelecidos.
+        */
+        
+    public:
+        string GetColuna();
+        /**
+        * obtem a coluna;
+        * retorna essa coluna.
+        */
+        void SetColuna(string);
+        /**
+        * define a coluna.
+        */
+};
