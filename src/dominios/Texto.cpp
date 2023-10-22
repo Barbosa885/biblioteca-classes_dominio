6ad1cb67cbfef16dd41c18213cae7e8398d4ef91
hidekitomiyama <145477920+hidekitomiyama@users.noreply.github.com>
2023-10-22 16:49:07 -0300

    Update Texto.cpp

--- src/dominios/Texto.cpp
+++ src/dominios/Texto.cpp
@@ -1,8 +1,19 @@
 #include "../../include/dominios_h/Textos.h"
 #include <string>
 #include <iostream>
+#include <regex>
 
-void TEXTO::isValid(string texto) {
+Texto::Texto()
+{
+    //ctor
+}
+
+Texto::~Texto()
+{
+    //dtor
+}
+
+void Texto::isValid(const string &texto) {
 /** 
  * @brief
 * verifica se o texto atende a alguns critérios específicos:
@@ -16,30 +27,38 @@ void TEXTO::isValid(string texto) {
 * @param o texto verificado
 * @throw exception_type: argumento invalido caso o texto não atenda um dos critérios estabelecidos.
 */
-    string pontuacao = ".,;!?";
-    if (!isupper(texto[0]))
-        throw invalid_argument("Argumento invalido");
-    if (texto.length() < 5 || texto.length() > 30)
-        throw invalid_argument("Argumento invalido");
-    for (int i = 1; i < texto.length(); i++) {
-        if (isalnum(texto[i]))
-            continue;
-        else if (find(pontuacao.begin(), pontuacao.end(), texto[i]) != pontuacao.end()) {
-            if (i != texto.length() -1){
-                if (find(pontuacao.begin(), pontuacao.end(), texto[i+1]) != pontuacao.end())
-                    throw invalid_argument("Argumento invalido");
-                if (texto[i] == ',' || texto[i] == ';')
-                    continue;
-                if (!isupper(texto[i+1]))
-                    throw invalid_argument("Argumento invalido");
-            }    
-        }
-        else if (texto[i] == ' ') {
-            if (i != texto.length() -1){
-                if (texto[i+1] == ' ')
-                    throw invalid_argument("Argumento invalido");
-                continue;
-            }
-        }
+
+void verificaDominioTexto(const std::string& texto) {
+    // Verifica se o texto tem entre 5 e 30 caracteres
+    if (texto.length() < 5 || texto.length() > 30) {
+        throw invalid_argument("Texto inválido")
     }
+
+    // Verifica se o primeiro caractere é uma letra maiúscula
+    if (!std::regex_search(texto, std::regex("^[A-Z]"))) {
+            throw invalid_argument("Texto inválido")
+    }
+
+    // Verifica se não há espaços em branco em sequência
+    if (std::regex_search(texto, std::regex("\\s{2,}"))) {
+            throw invalid_argument("Texto inválido")
+    }
+
+    // Verifica se não há sinais de pontuação em sequência, exceto vírgula ou ponto-e-vírgula
+    if (std::regex_search(texto, std::regex("[.,;?!]{2,}"))) {
+            throw invalid_argument("Texto inválido")
+    }
+
+    // Verifica se não há acentuação (caracteres especiais)
+    if (std::regex_search(texto, std::regex("[áéíóúÁÉÍÓÚàèìòùÀÈÌÒÙâêîôûÂÊÎÔÛãẽĩõũÃẼĨÕŨäëïöüÄËÏÖÜ]"))) {
+            throw invalid_argument("Texto inválido")
+    }
+
+    // Verifica se o primeiro caractere após sinal de pontuação (exceto vírgula ou ponto-e-vírgula) é letra maiúscula
+    if (std::regex_search(texto, std::regex("[.,;?!][a-z]"))) {
+            throw invalid_argument("Texto inválido")
+    }
+
+    // Se todas as verificações passaram, o texto é válido
+    cout << "Texto válido" << endl;
 }
