7dd92f1492b21676aff821344b33e4c50770be65
hidekitomiyama <145477920+hidekitomiyama@users.noreply.github.com>
2023-10-22 09:36:54 -0300

    Update Limite.h

--- include/dominios_h/Limite.h
+++ include/dominios_h/Limite.h
@@ -1,1 +1,21 @@
-
+class LIMITE{
+    private:
+        int limite;
+        void verifica(int);
+        /**
+        * verifica se o limite atende a alguns critérios específicos.
+        * @param limite verificado.
+        * saída: "argumento inválido" caso o limite não atenda a um dos críterios estabelecidos.
+        */
+        
+    public:
+        int GetLimite();
+        /**
+        * obtem o limite;
+        * retorna esse limite.
+        */
+        void SetLimite(int);
+        /**
+        * define o limite.
+        */
+};
