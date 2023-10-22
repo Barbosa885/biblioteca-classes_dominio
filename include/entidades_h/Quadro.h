e37f183a24cdc59ef232c16e9ff680faeb8d10b9
hidekitomiyama <145477920+hidekitomiyama@users.noreply.github.com>
2023-10-22 20:21:10 -0300

    Update Quadro.h

--- include/entidades_h/Quadro.h
+++ include/entidades_h/Quadro.h
@@ -1,23 +1,54 @@
-#ifndef ENTIDADES_H_INCLUDED
-#define ENTIDADES_H_INCLUDED
+#ifndef QUADRO_H_INCLUDED
+#define QUADRO_H_INCLUDED
 
-#include "dominios.h"
+#include "../dominios_h/Codigo.h"
+#include "../dominios_h/Texto.h"
+#include "../dominios_h/Limite.h"
 #include <string>
 
 using namespace std;
 
 class Quadro {
     private:
-            Dominio nomeAtributo;                   // Declarar cada atributo da classe.
+        Codigo codigo;
+        Texto nome;
+        Texto descricao;
+        Limite limite;
+    
     public:
-            void setnomeAtributo(const Dominio&);   // Declarar método set para cada atributo.
-            Dominio getnomeAtributo() const;        // Declarar método get para cada atributo.
-};
+        void setCodigo(const Codigo&);
+        void setNome(const Texto&);
+        void setDescricao(const Texto&);
+        void setLimite(const Limite&); 
+        Codigo getCodigo() const;    
+        Texto getNome() const;         
+        Texto getDescricao() const;         
+        Limite getLimite() const;        
+}; 
 
-inline void Entidade::setnomeAtributo(const Dominio &nomeAtributo){
-    this->nomeAtributo = nomeAtributo;
+inline void Quadro::setCodigo(const Codigo &codigo){
+    this->codigo = codigo;
+}
+inline void Quadro::setNome(const Texto &nome){
+    this->nome = nome;
+}
+inline void Quadro::setDescricao(const Texto &descricao){
+    this->descricao = descricao;
+}
+inline void Quadro::setLimite(const Limite &limite){
+    this->limite = limite;
+}
+inline Codigo Quadro::getCodigo() const{
+    return codigo;
+}
+inline Texto Quadro::getNome() const{
+    return nome;
+}
+inline Texto Quadro::getDescricao() const{
+    return descricao;
+}
+inline Coluna Quadro::getLimite() const{
+    return limite;
 }
 
-inline Dominio Entidade::getnomeAtributo() const{
-    return nomeAtributo;
-}
+#endif // QUADRO_H_INCLUDED
