1555ba24b48ae4b80d263702e7709d0edd7fbe37
hidekitomiyama <145477920+hidekitomiyama@users.noreply.github.com>
2023-10-22 20:01:14 -0300

    Create TUCartao.cpp

---
+++ src/testes/TUCartao.cpp
@@ -0,0 +1,31 @@
+#include "../testes_h/TUCartao.h"
+
+void TUCartao::setUp(){
+    cartao = new Cartao();
+    estado = SUCESSO;
+}
+
+void TUCartao::tearDown(){
+    delete cartao;
+}
+
+void TUCartao::testarCenarioSucesso(){
+    Codigo codigo;
+    codigo.setValor(VALOR_VALIDO);
+    cartao->setCodigo(codigo);
+    if(cartao->getCodigo().getValor() != VALOR_VALIDO)
+        estado = FALHA;
+
+    Prioridade prioridade;
+    prioridade.setValor(VALOR_VALIDO);
+    cartao->setPrioridade(prioridade);
+    if(cartao->getPrioridade().getValor() != VALOR_VALIDO)
+        estado = FALHA;
+}
+
+int TUCartao::run(){
+    setUp();
+    testarCenarioSucesso();
+    tearDown();
+    return estado;
+}
