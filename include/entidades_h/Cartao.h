0066cb2dbce8169403c2ca138116e44357c0ae04
hidekitomiyama <145477920+hidekitomiyama@users.noreply.github.com>
2023-10-22 19:17:53 -0300

    Create Cartao.h

---
+++ include/entidades_h/Cartao.h
@@ -0,0 +1,140 @@
+#ifndef ENTIDADES_H_INCLUDED
+#define ENTIDADES_H_INCLUDED
+
+#include "dominios.h"
+#include <string>
+
+using namespace std;
+
+/** 
+ * @brief Classe Cartao.
+ * 
+ * Esta classe é usada para manipular um cartão que contém um código, nome, descrição e coluna.
+ */
+class Cartao {
+    private:
+        /** 
+         * @brief Variável de código.
+         * 
+         * Esta variável armazena o código do cartão.
+         */
+        Codigo codigo;
+        
+        /** 
+         * @brief Variável de nome.
+         * 
+         * Esta variável armazena o nome do cartão.
+         */
+        Texto nome;
+        
+        /** 
+         * @brief Variável de descrição.
+         * 
+         * Esta variável armazena a descrição do cartão.
+         */
+        Texto descricao;
+        
+        /** 
+         * @brief Variável de coluna.
+         * 
+         * Esta variável armazena a coluna do cartão.
+         */
+        Coluna coluna;
+    
+    public:
+        /** 
+         * @brief Método setCodigo.
+         * 
+         * Este método é usado para definir o valor da variável de código.
+         * @param Codigo O novo código do cartão.
+         */
+        void setCodigo(const Codigo&);
+        
+        /** 
+         * @brief Método setNome.
+         * 
+         * Este método é usado para definir o valor da variável de nome.
+         * @param Texto O novo nome do cartão.
+         */
+        void setNome(const Texto&);
+        
+        /** 
+         * @brief Método setDescricao.
+         * 
+         * Este método é usado para definir o valor da variável de descrição.
+         * @param Texto A nova descrição do cartão.
+         */
+        void setDescricao(const Texto&);
+        
+        /** 
+         * @brief Método setColuna.
+         * 
+         * Este método é usado para definir o valor da variável de coluna.
+         * @param Coluna A nova coluna do cartão.
+         */
+        void setColuna(const Coluna&);
+        
+        /** 
+         * @brief Método getCodigo.
+         *
+         * Este método é usado para obter o valor da variável de código.  
+         *
+         * @return Retorna o código do cartão.  
+         */  
+        Codigo getCodigo() const;  
+        
+        /**   
+          *
+          *@brief Método getNome.  
+          *
+          *@return Retorna o nome do cartão.  
+          */  
+        Texto getNome() const;        
+        
+        /**   
+          *
+          *@brief Método getDescricao.  
+          *
+          *@return Retorna a descrição do cartão.  
+          */  
+        Texto getDescricao() const;        
+        
+        /**   
+          *
+          *@brief Método getColuna.  
+          *
+          *@return Retorna a coluna do cartão.  
+          */  
+        Coluna getColuna() const;        
+};
+
+/**   
+  *
+  *@brief Implementação inline dos métodos setters e getters da classe Cartao.  
+  *
+  */  
+
+inline void Cartao::setCodigo(const Codigo &codigo){
+    this->codigo = codigo;
+}
+inline void Cartao::setNome(const Texto &nome){
+    this->nome = nome;
+}
+inline void Cartao::setDescricao(const Texto &descricao){
+    this->descricao = descricao;
+}
+inline void Cartao::setColuna(const Coluna &coluna){
+    this->coluna = coluna;
+}
+inline Codigo Cartao::getCodigo() const{
+    return codigo;
+}
+inline Texto Cartao::getNome() const{
+    return nome;
+}
+inline Texto Cartao::getDescricao() const{
+    return descricao;
+}
+inline Coluna Cartao::getColuna() const{
+    return coluna;
+}
