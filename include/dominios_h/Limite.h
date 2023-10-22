0ce2d2771274fd446e8299c9be07d87f3b4efcc0
hidekitomiyama <145477920+hidekitomiyama@users.noreply.github.com>
2023-10-22 11:29:01 -0300

    Update Limite.h

--- include/dominios_h/Limite.h
+++ include/dominios_h/Limite.h
@@ -1,30 +1,49 @@
+/**
+ * @file Limite.h
+ * @brief Definição da classe Limite.
+ *
+ * @brief Classe que representa um limite.
+ *
+ * Autor: Hideki Tomiyama (190014351)
+ */
+
 #ifndef LIMITE_H
 #define LIMITE_H
+
+/**
+ * @file LIMITE.h
+ * @brief Declaração da classe LIMITE.
+ */
 
 #include <string>
 #include <stdexcept>
 
 using namespace std;
 
-class LIMITE{
-    private:
-        int limite;
-        void isValid(int);
-        /**
-        * verifica se o limite atende a alguns critérios específicos.
-        * @param limite verificado.
-        * saída: "argumento inválido" caso o limite não atenda a um dos críterios estabelecidos.
-        */
-        
-    public:
-        /**
-        * obtem o limite;
-        * retorna esse limite.
-        */
-        int GetLimite();
+class LIMITE {
+private:
+    int limite; /**< Variável que armazena o limite. */
+
+    /**
+     * @brief Verifica se um valor é válido como limite.
+     * @param value O valor a ser verificado.
+     */
+    void isValid(int value);
+
+public:
+
+    /**
+     * @brief Obtém o valor do limite.
+     * @return O valor do limite.
+     */
+    int GetLimite();
 };
 
-inline int LIMITE::GetLimite(){
+/**
+ * @brief Função inline para obter o valor do limite.
+ * @return O valor do limite.
+ */
+inline int LIMITE::GetLimite() {
     return limite;
 }
 
