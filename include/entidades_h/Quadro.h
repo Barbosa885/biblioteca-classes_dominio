7ba26cd29e2b58d296157246000ae02954b1495d
hidekitomiyama <145477920+hidekitomiyama@users.noreply.github.com>
2023-10-22 20:16:22 -0300

    Create Quadro.h

---
+++ include/entidades_h/Quadro.h
@@ -0,0 +1,23 @@
+#ifndef ENTIDADES_H_INCLUDED
+#define ENTIDADES_H_INCLUDED
+
+#include "dominios.h"
+#include <string>
+
+using namespace std;
+
+class Quadro {
+    private:
+            Dominio nomeAtributo;                   // Declarar cada atributo da classe.
+    public:
+            void setnomeAtributo(const Dominio&);   // Declarar método set para cada atributo.
+            Dominio getnomeAtributo() const;        // Declarar método get para cada atributo.
+};
+
+inline void Entidade::setnomeAtributo(const Dominio &nomeAtributo){
+    this->nomeAtributo = nomeAtributo;
+}
+
+inline Dominio Entidade::getnomeAtributo() const{
+    return nomeAtributo;
+}
