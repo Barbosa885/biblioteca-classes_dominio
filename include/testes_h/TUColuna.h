f2198d0291c3b62d8798fb26055945b9103e299c
hidekitomiyama <145477920+hidekitomiyama@users.noreply.github.com>
2023-10-22 11:26:01 -0300

    Update TUColuna.h

--- include/testes_h/TUColuna.h
+++ include/testes_h/TUColuna.h
@@ -1,1 +1,20 @@
+class TUCOLUNA {
+private:
+    const string COLUNA_VALIDA = "CONCLUIDO"; /**< Constante que representa uma coluna valida para o teste. */
+    const string COLUNA_INVALIDA = "Conclui"; /**< Constante que representa uma coluna invalida para o teste. */
+    COLUNA *coluna;
+    int estado; /**< Variável que armazena o estado do teste. */
+    /**
+    * Métodos privados
+    */
+    void SetUp(); /**< Método para configurar o ambiente de teste. */
+    void TearDown();  /**< Método para limpar o ambiente de teste. */
+    void TestarCenarioSucesso(); /**< Método para testar o cenário de sucesso. */
+    void TestarCenarioFalha(); /**< Método para testar o cenário de falha. */    
 
+public:
+    const static int SUCESSO =  0;  /**< Constante que representa o estado de sucesso do teste. */
+    const static int FALHA = -1; /**< Constante que representa o estado de falha do teste. */
+    /**< Metodo publico */
+    int Run(); /**< Método para executar os testes de unidade. */
+};
