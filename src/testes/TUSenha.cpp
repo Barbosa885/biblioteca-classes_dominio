ee530e8175c72a5b8ce34f213017bc61854396a4
hidekitomiyama <145477920+hidekitomiyama@users.noreply.github.com>
2023-10-22 16:22:03 -0300

    Update TUSenha.cpp

--- src/testes/TUSenha.cpp
+++ src/testes/TUSenha.cpp
@@ -1,20 +1,51 @@
-void TUSENHA::SetUp(){  /** Configura o ambiente de teste. */
+#include <iostream>
+#include "../../include/testes_h/TUSenha.h"
+#include "../../include/dominios_h/Senha.h"
+
+TUSenha::TUSenha()
+{
+    //ctor    
+}
+
+TUSenha::~TUSenha()
+{
+    //dtor
+}
+
+using namespace std;
+
+const string TUSenha::SENHAS_VALIDAS[] = {
+    "Abc1.",
+    "Zy?x2"
+};
+
+const string TUSenha::SENHAS_INVALIDAS[] = {
+    "abc.1",
+    "ABC?2",
+    "Abc!e",
+    "Abcd3",
+    "Abb,4"
+};
+
+void TUSenha::setUp(){  /** Configura o ambiente de teste. */
     senha = new SENHA();
     estado = SUCESSO;
 }
 
-void TUSENHA::TearDown(){ /** Limpa o ambiente de teste, excluindo a instância SENHA. */
+void TUSenha::tearDown(){ /** Limpa o ambiente de teste, excluindo a instância SENHA. */
     delete senha;
 }
 
-void TUSENHA::TestarCenarioSucesso(){ /** Testa um cenário de sucesso em que a senha é definida como uma senha válida. */
-    try{
-        senha->SetSenha(SENHA_VALIDA);
-        if (senha->GetSenha() != SENHA_VALIDA)
+void TUSenha::testarCenarioSucesso(){ /** Testa um cenário de sucesso em que a senha é definida como uma senha válida. */
+    for (const string &senhaValida : SENHAS_VALIDAS){
+        try{
+            senha->setSenha(SENHA_VALIDA);
+            if (senha->getSenha() != SENHA_VALIDA)
+                estado = FALHA;
+        }
+        catch(invalid_argument &excecao){
             estado = FALHA;
-    }
-    catch(invalid_argument &excecao){
-        estado = FALHA;
+        }
     }
 }
 
