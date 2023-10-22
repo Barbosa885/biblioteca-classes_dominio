2dc3b230517e8c977f11ee31bcb89cfcfb5c143d
hidekitomiyama <145477920+hidekitomiyama@users.noreply.github.com>
2023-10-22 20:41:53 -0300

    Update TUCartao.h

--- include/testes_h/TUCartao.h
+++ include/testes_h/TUCartao.h
@@ -9,20 +9,40 @@
 
 using namespace std;
 
+/**
+ * @class TUCartao
+ * @brief Classe de teste para a classe Cartao.
+ */
 class TUCartao {
 private:
-    const static string CODIGO_VALIDO = "AB12"; 
-    const static string NOME_VALIDO = "Texto valido"; 
-    const static string DESCRICAO_VALIDA = "Descricao valida"; 
-    const static string COLUNA_VALIDA = "SOLICITADO"; 
-    Cartao *cartao;
-    int estado;                             
+    const static string CODIGO_VALIDO; ///< Código válido para teste
+    const static string NOME_VALIDO; ///< Nome válido para teste
+    const static string DESCRICAO_VALIDA; ///< Descrição válida para teste
+    const static string COLUNA_VALIDA; ///< Coluna válida para teste
+    Cartao *cartao; ///< Ponteiro para o objeto Cartao a ser testado
+    int estado; ///< Estado do teste
+
+    /**
+     * @brief Configura o ambiente de teste.
+     */
     void setUp();                           
+
+    /**
+     * @brief Limpa o ambiente de teste.
+     */
     void tearDown();                        
+
+    /**
+     * @brief Testa um cenário de sucesso.
+     */
     void testarCenarioSucesso();            
 public:
-    const static int SUCESSO =  0;          
-    const static int FALHA   = -1;          
+    const static int SUCESSO; ///< Indica que o teste foi bem-sucedido
+    const static int FALHA; ///< Indica que o teste falhou
+
+    /**
+     * @brief Executa o teste.
+     */
     void run();                              
 };
 
